ac56a9162f8f88a71ff86eb87080ad283fe891a9
vivski <vnls1@aluno.ifal.edu.br>
2024-05-12 21:01:48 -0300

    RegisterModal; modals
    terminada a tela de registro; começa a usar o prisma: npm i -D prisma; npx prisma init; instalar extensão prisma

--- src/app/componentes/modals/RegisterModal.tsx
+++ src/app/componentes/modals/RegisterModal.tsx
@@ -100,7 +100,24 @@ const RegisterModal = () => {
         label="Continue with Github"
         icon={AiFillGithub}
         onClick={()=> {}}/>
-
+      <div className="
+      text-neutral-500
+      text-center
+      mt-4
+      font-light
+      ">
+      <div className="justify-center flex flex-row items-center gap-2">
+        <div> Already have an account? </div>
+        <div
+        onClick={registerModal.onClose}
+        className="
+        text-neutral-800 
+        cursor-pointer
+        hover:underline">
+        Log in
+        </div>
+      </div>
+      </div>
       </div>
     )
 // utualizar o Modal.
--- src/app/componentes/modals/modals.tsx
+++ src/app/componentes/modals/modals.tsx
@@ -63,6 +63,7 @@ const Modal: React.FC<ModalProps> = ({
  
 
 
+
     return (
         <>
         <div
