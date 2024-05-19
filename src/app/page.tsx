b4f36a84f11720927d93a9418e45bd03c63fb4c1
vivski <vnls1@aluno.ifal.edu.br>
2024-05-19 20:40:46 -0300

    Category
    category section - unfinished.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
         ">
 
         <Card 
-        imageSrc="/images/img/ouropreto.jpg"
+        imageSrc="https://github.com/vivski/airbnbproject/blob/master/public/images/img/ouropreto.jpg"
         city="Ouro Preto"
         country="Brasil"
         type="Countryside"
@@ -30,6 +30,41 @@ export default function Home() {
         imageSrc="/images/img/ouropreto.jpg"
         city="Cusco"
         country="Peru"
+        type="Countryside"
+        price="R$ 2000.00" />
+
+        <Card 
+        imageSrc="https://github.com/vivski/airbnbproject/blob/master/public/images/img/riodejaneiro.jpg"
+        city="Rio de Janeiro"
+        country="Brasil"
+        type="Beach"
+        price="R$ 2000.00" />
+
+        <Card 
+        imageSrc="https://github.com/vivski/airbnbproject/blob/master/public/images/img/santiagochile.jpg"
+        city="Santiago"
+        country="Chile"
+        type="Countryside"
+        price="R$ 2000.00" />
+
+        <Card 
+        imageSrc="https://github.com/vivski/airbnbproject/blob/master/public/images/img/maceio.jpg"
+        city="Maceio"
+        country="Brasil"
+        type="Countryside"
+        price="R$ 2000.00" />
+
+        <Card 
+        imageSrc="https://github.com/vivski/airbnbproject/blob/master/public/images/img/limaperu.jpg"
+        city="Lima"
+        country="Peru"
+        type="Countryside"
+        price="R$ 2000.00" />
+
+        <Card 
+        imageSrc="https://github.com/vivski/airbnbproject/blob/master/public/images/img/buenosairesargentina.jpg"
+        city="Buenos Aires"
+        country="Argentina"
         type="Countryside"
         price="R$ 2000.00" />
 
